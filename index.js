cf8704ac010d4aaa2983129d83e1a0d58ae754e1
Christian van Ooy <cvodutch@gmail.com>
2018-07-11 01:16:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments) {
   var array = [];
-    for (var i = 0; i < musicians.length; i++){
-      array.push(musicians[i] +"plays"+ instruments[i]);
-    }
+  for (let i = 0; i < musicians.length; i ++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  };
   return array;
-}
+};
 
-function johnLennonFacts(facts){
-  var array =[]
-  for(var i=0; i< facts.length;i++){
-    array.push(facts[i] + "!!!")
-  }
-  return array
-}
+function johnLennonFacts(array) {
+  var i = 0;
+  while (i < array.length) {
+    array[i] += "!!!";
+    i++;
+  };
+  return array;
+};
 
 function iLoveTheBeatles(number) {
-  var array =[]
-  do{
-    array.push("I love the Beatles!")
-    number++;
-  }
-    while (number < 15)
-
-    return array
-}
+  var array = [];
+  var i = 0;
+  do {
+    array.push("I love the Beatles!");
+    i++;
+  } while (i <= number ++ number < 15);
+  return array;
+};
