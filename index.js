d4b6a205464a2e63d9ef9476fcc402714de0fafb
Christian van Ooy <cvodutch@gmail.com>
2018-07-11 01:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function iLoveTheBeatles(number) {
   do {
     array.push("I love the Beatles!");
     i++;
-  } while (i <= number ++ number < 15);
+  } while (i <= number && number < 15);
   return array;
 };
